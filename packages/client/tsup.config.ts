7b86958830170d1b8badc2db5d489dc3aeb0d532
demenskyi <yaroslav.demenskiy@gmail.com>
2024-08-07 11:58:52 -0700

    Dependencies updated

--- packages/client/tsup.config.ts
+++ packages/client/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig([
     entry: ["./src/index.ts"],
     outDir: "./dist",
     format: ["cjs", "esm"],
-    external: ["mysql2"],
+    external: ["@singlestore/ai", "mysql2"],
     dts: true,
     sourcemap: true,
   },
