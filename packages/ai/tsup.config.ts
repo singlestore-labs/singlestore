653269d1c9a1080129a865b82e857d67b7338cfb
demenskyi <yaroslav.demenskiy@gmail.com>
2024-09-18 18:01:09 -0700

    Imports fix

--- packages/ai/tsup.config.ts
+++ packages/ai/tsup.config.ts
@@ -2,13 +2,32 @@ import { defineConfig } from "tsup";
 
 export default defineConfig([
   {
-    entry: {
-      "index": "./src/index.ts",
-      "embeddings": "./src/embeddings/index.ts",
-      "chat-completions/index": "./src/chat-completions/index.ts",
-      "chat-completions/errors": "./src/chat-completions/errors/index.ts",
-    },
+    entry: ["./src/index.ts"],
     outDir: "./dist",
+    format: ["cjs", "esm"],
+    external: ["openai"],
+    dts: true,
+    sourcemap: true,
+  },
+  {
+    entry: ["./src/embeddings/index.ts"],
+    outDir: "./dist/embeddings",
+    format: ["cjs", "esm"],
+    external: ["openai"],
+    dts: true,
+    sourcemap: true,
+  },
+  {
+    entry: ["./src/chat-completions/index.ts"],
+    outDir: "./dist/chat-completions",
+    format: ["cjs", "esm"],
+    external: ["openai", "zod", "zod-to-json-schema"],
+    dts: true,
+    sourcemap: true,
+  },
+  {
+    entry: ["./src/chat-completions/errors/index.ts"],
+    outDir: "./dist/chat-completions/errors",
     format: ["cjs", "esm"],
     external: ["openai", "zod", "zod-to-json-schema"],
     dts: true,
