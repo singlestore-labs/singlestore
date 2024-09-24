8b4a6f273181621a94ef93d39c97ab3f50d95023
demenskyi <yaroslav.demenskiy@gmail.com>
2024-09-24 13:50:31 +0200

    Client: tsup config updated

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -5,7 +5,6 @@ export default defineConfig([
     entry: ["./src/index.ts"],
     outDir: "./dist",
     format: ["cjs", "esm"],
-    external: ["openai", "zod", "zod-to-json-schema"],
     dts: true,
     sourcemap: true,
   },
