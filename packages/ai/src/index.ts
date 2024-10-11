9b89924dafc2bed1af3d4c225503f63a164ba129
demenskyi <yaroslav.demenskiy@gmail.com>
2024-10-11 14:03:47 -0700

    AI: TextSplitter added

--- packages/ai/src/index.ts
+++ packages/ai/src/index.ts
@@ -2,4 +2,5 @@ export type * from "./types";
 export { ChatCompletionsManager, ChatCompletionTool } from "./chat-completions";
 export { EmbeddingsManager } from "./embeddings";
 export { MessageLengthExceededError, MessagesLengthExceededError, parseLengthErrorMessage } from "./chat-completions/errors";
+export { TextSplitter } from "./text-splitter";
 export * from "./ai";
--- packages/ai/src/types.ts
+++ packages/ai/src/types.ts
@@ -1,3 +1,4 @@
 export type * from "./chat-completions";
 export type * from "./chat-completions/errors";
 export type * from "./embeddings";
+export type * from "./text-splitter";
